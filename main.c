45ae854d13be04824107b94842fd12ed0037badb
Sandro <sandro.alder@juventus.schule>
2024-10-18 12:47:09 +0200

    V1.2

--- main.c
+++ main.c
@@ -1,51 +1,75 @@
 // Getraenke Automat Version 0.1
 
 
+#include <math.h>
 #include <stdio.h>
+
+void Menu_anzeigen(void) {
+    printf("\nBitte waehlen sie ihr Getraenk.\n");
+    printf("1 = Wasser (0,50 Euro)\n");
+    printf("2 = Limonade (1,00 Euro)\n");
+    printf("3 = Bier (2,00 Euro)\n");
+}
+
 
 int main() {
 
+    float Preis = 0;
     int einwurf=0;
     int Wahl=0;
-    int Ausgabe=0;
+    int Menge=0;
 
-    printf("\n Getraenke Automat | Bitte werfen sie 1 Euro ein:\n ");
-    scanf("%d", &einwurf);
+    while(1) {
 
-    // überpruefe Geldstueck
-    if(einwurf == 1) {
-        Ausgabe = 1;
-        printf  ("\n Bitte waehlen sie ihr Getraenk.\n");
-        ("\n 1 = Wasser\n");
-        ("\n 2 = Limonade\n");
-        ("\n 3 = Bier\n");
+        Preis = 0;
+        einwurf=0;
+        Wahl=0;
+
+        Menu_anzeigen();
         scanf("%d",&Wahl);
+        printf("Bitte Menge waehlen\n");
+        scanf("%d",&Menge);
 
-        while (Ausgabe)
-        {
-            switch (Wahl) {
-
-                case 1:
-                    printf("\n Wasser ausgeben\n");
-                Ausgabe = 0;
-                break;
-                case 2:
-                    printf("\n Limonade ausgeben\n");
-                Ausgabe = 0;
-                break;
-                case 3:
-                    printf("\n Wasser ausgeben\n");
-                Ausgabe = 0;
-                break;
-                default:
-                    printf("\n Eingabe ungueltig. Bitte Wahl erneut treffen.\n");
-                break;
-            }
+        switch (Wahl) {
+            case 1:
+                Preis = 0.5;
+            break;
+            case 2:
+                Preis = 1;
+            break;
+            case 3:
+                Preis = 2;
+            break;
         }
 
-        printf("\n Vielen Dank, bitte entnehmen sie ihr Getraenk.\n");
-    }
-    else {
-        printf("\n Sie haben kein 1 Euro Stueck eingeworfen.\n");
+        Preis = Preis * Menge;
+        printf("\nBitte werfen sie %.2f Euro ein:\n",Preis);
+
+        while (Preis > 0) {
+            scanf("%d",&einwurf);
+            Preis -= einwurf;
+            einwurf = 0;
+        }
+
+        switch (Wahl) {
+
+            case 1:
+                printf("\n%.2dx Wasser ausgeben\n",Menge);
+            break;
+            case 2:
+                printf("\n%.2dx Limonade ausgeben\n",Menge);
+            break;
+            case 3:
+                printf("\n%.2dx Bier ausgeben\n",Menge);
+            break;
+        }
+
+        if (Preis < 0) {
+            Preis = (Preis) * -1;
+            printf("\n%.2f Euro Rueckgeld ausgeben\n",Preis);
+        }
+
+        printf("\nVielen Dank, bitte entnehmen sie ihr Getraenk.\n");
+
     }
 }
